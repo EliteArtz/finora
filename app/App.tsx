45b2bfa8a6c6a3026a217f7139960f5d821fb529
Jordan Brunn <redstonetechnic8@gmail.com>
2025-04-17 23:46:16 +0200

    setup fontawesome

--- app/App.tsx
+++ app/App.tsx
@@ -4,11 +4,22 @@ import styled, {css, ThemeProvider} from 'styled-components/native'
 import Label from "../components/Label/Label";
 import theme from "../assets/style/theme";
 
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {far} from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+
+import Constants from 'expo-constants';
+
+library.add(fab, fas, far)
+
 const Style_App = styled.View`
   display: flex;
   flex: 1;
   ${({ theme }) => css`
     background-color: ${theme.color.background};
+    padding-top: ${Constants.statusBarHeight + theme.size.xl.value * 16};
   `}
 `;
 
@@ -17,7 +28,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
       <Style_App>
-        <Label>Text</Label>
+        <Label>Test <FontAwesomeIcon icon="check" /></Label>
       </Style_App>
     </ThemeProvider>
   );
